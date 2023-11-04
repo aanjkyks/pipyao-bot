9d6ce20452bcd121bba48b1ada7c12c83167846f
aanjkyks <ainis.antans@gmail.com>
2023-11-04 20:58:01 +0200

    check for channel correctly, style improvements

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 import os
+import random
+import sys
 
 import discord
-import random
 
 intents = discord.Intents.default()
 intents.voice_states = True
 client = discord.Client(intents=intents)
 
 
-lobby_channel_name = "Lobby"
-voice_channel_category_name = "Voice Channels"
+LOBBY_CHANNEL_NAME = "Lobby"
+VOICE_CHANNLE_CATEGORY_NAME = "Voice Channels"
 channel_names = [
     "илья",
     "сергей",
@@ -33,27 +34,27 @@ async def on_ready():
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    if before is not None:
-        if before.channel.name != lobby_channel_name:
+    if before.channel is not None:
+        if before.channel.name != LOBBY_CHANNEL_NAME:
             channel = before.channel
             if len(channel.members) == 0:
                 await channel.delete()
-    if after is not None:
-        if after.channel.name != lobby_channel_name:
+    if after.channel is not None:
+        if after.channel.name != LOBBY_CHANNEL_NAME:
             return
-    guild = after.channel.guild
-    voice_channel_category = discord.utils.find(
-        lambda cat: cat.name == voice_channel_category_name, guild.categories
-    )
-    new_channel = await guild.create_voice_channel(
-        random.choice(channel_names), category=voice_channel_category
-    )
-    await member.move_to(new_channel)
+        guild = after.channel.guild
+        voice_channel_category = discord.utils.find(
+            lambda cat: cat.name == VOICE_CHANNLE_CATEGORY_NAME, guild.categories
+        )
+        new_channel = await guild.create_voice_channel(
+            random.choice(channel_names), category=voice_channel_category
+        )
+        await member.move_to(new_channel)
 
 
 if __name__ == "__main__":
     token = os.environ["BOT_TOKEN"]
     if token is None:
         print("token not configured")
-        exit(1)
+        sys.exit(1)
     client.run(token)
